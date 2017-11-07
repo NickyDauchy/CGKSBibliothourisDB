433e2960e63bc3c62021e1ed54c59165a401b087
Gerri Vanderhaegen <gerri.vanderhaegen@cegeka.com>
2017-11-07 10:15:11 +0100

    equals of borrowedBooks

--- src/test/java/be/cegeka/bibliothouris/domain/books/BorrowedBookTest.java
+++ src/test/java/be/cegeka/bibliothouris/domain/books/BorrowedBookTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class BorrowedBookTest {
     private BorrowedBook borrowedBook;
 
@@ -16,26 +18,106 @@ public class BorrowedBookTest {
 
     @Test
     public void getBookIdOfBorrowedBook() throws Exception {
-        Assertions.assertThat(borrowedBook.getBookid()).isEqualTo(1);
+        assertThat(borrowedBook.getBookid()).isEqualTo(1);
     }
 
     @Test
     public void getUserIdOfBorrowedBook() throws Exception {
-        Assertions.assertThat(borrowedBook.getUserid()).isEqualTo(1);
+        assertThat(borrowedBook.getUserid()).isEqualTo(1);
     }
 
     @Test
     public void getIdOfBorrowedBook() throws Exception {
-        Assertions.assertThat(borrowedBook.getId()).isEqualTo(borrowedBook.getId());
+        assertThat(borrowedBook.getId()).isEqualTo(borrowedBook.getId());
     }
 
     @Test
     public void getStartDateOfBorrowedBook() throws Exception {
-        Assertions.assertThat(borrowedBook.getStartdate()).isEqualTo(java.sql.Date.valueOf(LocalDate.now()));
+        assertThat(borrowedBook.getStartdate()).isEqualTo(java.sql.Date.valueOf(LocalDate.now()));
     }
 
     @Test
     public void getDueDateOfBorrowedBook() throws Exception {
-        Assertions.assertThat(borrowedBook.getDuedate()).isEqualTo(java.sql.Date.valueOf(LocalDate.now().plusWeeks(3)));
+        assertThat(borrowedBook.getDuedate()).isEqualTo(java.sql.Date.valueOf(LocalDate.now().plusWeeks(3)));
+    }
+
+    @Test
+    public void name() throws Exception {
+    }
+    @Test
+    public void equals_SameBook_isTrue() throws Exception {
+        assertThat(borrowedBook).isEqualTo(borrowedBook);
+    }
+
+    @Test
+    public void equals_BookWithSameBookIDUserIDBorrowDateAndDueDate_isTrue() throws Exception {
+        assertThat(borrowedBook).isEqualTo(new BorrowedBook(1,1, java.sql.Date.valueOf(LocalDate.now()),java.sql.Date.valueOf(LocalDate.now().plusWeeks(3))));
+    }
+
+    @Test
+    public void equals_NullObject_isFalse() throws Exception {
+        assertThat(borrowedBook).isNotEqualTo(null);
+    }
+
+    @Test
+    public void equals_ifOtherClass_isFalse() throws Exception {
+        assertThat(borrowedBook).isNotEqualTo(new Object());
+    }
+
+    @Test
+    public void equals_BookWithOtherBookID_isFalse() throws Exception {
+        assertThat(borrowedBook).isNotEqualTo(new BorrowedBook(2,1, java.sql.Date.valueOf(LocalDate.now()),java.sql.Date.valueOf(LocalDate.now().plusWeeks(3))));
+    }
+
+    @Test
+    public void equals_BookWithOtherUserID_isFalse() throws Exception {
+        assertThat(borrowedBook).isNotEqualTo(new BorrowedBook(1,2, java.sql.Date.valueOf(LocalDate.now()),java.sql.Date.valueOf(LocalDate.now().plusWeeks(3))));
+    }
+
+    @Test
+    public void equals_BookWithOtherStartDate_isFalse() throws Exception {
+        assertThat(borrowedBook).isNotEqualTo(new BorrowedBook(1,1, java.sql.Date.valueOf(LocalDate.now().plusDays(5)),java.sql.Date.valueOf(LocalDate.now().plusWeeks(3))));
+    }
+
+    @Test
+    public void equals_BookWithOtherDueDate_isFalse() throws Exception {
+        assertThat(borrowedBook).isNotEqualTo(new BorrowedBook(1,1, java.sql.Date.valueOf(LocalDate.now()),java.sql.Date.valueOf(LocalDate.now().plusWeeks(5))));
+    }
+
+
+    @Test
+    public void equals_ObjectWithNullStartDate_IsFalse() throws Exception {
+        assertThat(borrowedBook).isNotEqualTo(new BorrowedBook(1,1, null, java.sql.Date.valueOf(LocalDate.now().plusWeeks(3))));
+    }
+
+    @Test
+    public void equals_ObjectWithNullDueDate_IsFalse() throws Exception {
+        assertThat(borrowedBook).isNotEqualTo(new BorrowedBook(1,1, java.sql.Date.valueOf(LocalDate.now()),null));
+    }
+
+
+    @Test
+    public void equals_ObjectWithBothNullStartDate_IsTrue() throws Exception {
+        assertThat(new BorrowedBook(1,1, null, java.sql.Date.valueOf(LocalDate.now().plusWeeks(3)))).
+                isEqualTo(new BorrowedBook(1,1, null, java.sql.Date.valueOf(LocalDate.now().plusWeeks(3))));
+    }
+
+    @Test
+    public void equals_ObjectWithBothNullDueDate_IsTrue() throws Exception {
+        assertThat(new BorrowedBook(1,1, java.sql.Date.valueOf(LocalDate.now()),null)).
+                isEqualTo(new BorrowedBook(1,1, java.sql.Date.valueOf(LocalDate.now()),null));
+    }
+
+
+    @Test
+    public void equals_ObjectWithNoNullStartDate_IsFalse() throws Exception {
+        assertThat(new BorrowedBook(1,1, null,java.sql.Date.valueOf(LocalDate.now().plusWeeks(3)))).
+                isNotEqualTo(new BorrowedBook(1,1, java.sql.Date.valueOf(LocalDate.now()),java.sql.Date.valueOf(LocalDate.now().plusWeeks(3))));
+    }
+
+    @Test
+    public void equals_ObjectWithNoNullDueDate_IsFalse() throws Exception {
+        assertThat(new BorrowedBook(1,1, java.sql.Date.valueOf(LocalDate.now()),null)).
+                isNotEqualTo(new BorrowedBook(1,1, java.sql.Date.valueOf(LocalDate.now()),java.sql.Date.valueOf(LocalDate.now().plusWeeks(3))));
     }
 }
