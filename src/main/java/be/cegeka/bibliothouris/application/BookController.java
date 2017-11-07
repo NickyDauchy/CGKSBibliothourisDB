43e1aea6f2bdaa48a4674fed54ce764505a19103
Jeroen Stroobants <jeroen.stroobants@cegeka.com>
2017-11-07 13:57:16 +0100

    ISBN parameter to LowerCase (isbn) in BookController

--- src/main/java/be/cegeka/bibliothouris/application/BookController.java
+++ src/main/java/be/cegeka/bibliothouris/application/BookController.java
@@ -19,20 +19,20 @@ public class BookController {
 
     @GetMapping(path = "/searchBookByISBN")
     @Secured("ROLE_USER")
-    public List<Book> searchBookByISBN(@RequestParam(value = "ISBN", required = true) String ISBN) {
-        return bookService.searchBookByISBN(ISBN);
+    public List<Book> searchBookByISBN(@RequestParam(value = "isbn", required = true) String isbn) {
+        return bookService.searchBookByISBN(isbn);
     }
 
     @PostMapping(path = "/addBook")
     @Secured("ROLE_LIBRARIAN")
-    public void addBook(@RequestParam(value = "isbn", required = true) String ISBN,
+    public void addBook(@RequestParam(value = "isbn", required = true) String isbn,
                         @RequestParam(value = "title", required = true) String title,
                         @RequestParam(value = "authorLastName", required = true) String authorLastName,
                         @RequestParam(value = "authorFirstName", required = false) String authorFirstName) {
         if (authorFirstName != null) {
-            bookService.addBook(ISBN, title, authorLastName, authorFirstName);
+            bookService.addBook(isbn, title, authorLastName, authorFirstName);
         } else {
-            bookService.addBook(ISBN, title, authorLastName);
+            bookService.addBook(isbn, title, authorLastName);
         }
     }
 
